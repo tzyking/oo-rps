466f68082b03badf745a8e77125600da0d5de790
wynnemlo <wynnemlo@gmail.com>
2014-10-25 13:57:34 +0800

    Refactored according to solutions

--- rps.rb
+++ rps.rb
@@ -3,42 +3,66 @@
 # Command-line Rock Paper Scissors game implemented in Ruby with OOP.
 # User plays against the compputer that randomly genereates its moves.
 
-class Computer
-	attr_accessor :name, :move
+# TODO: new class Hand that uses include Comparable
 
-	def initialize
-		@name = "Computer"
+class Hand
+	attr_accessor :move
+
+	include Comparable
+
+	def initialize(move)
+		@move = move
 	end
 
-	def make_move
-		moves = ["P", "R", "S"]
-		self.move = moves.sample
+	def <=>(another_hand)
+		if self.move == another_hand.move
+			0
+		elsif (self.move == "P" && another_hand.move == "R") || (self.move == "R" && another_hand.move == "S") ||
+			(self.move == "S" && another_hand.move == "P")
+			1
+		else
+			-1
+		end
+	end
+
+end
+
+class Player
+	attr_accessor :name, :hand
+
+	def initialize(n)
+		@name = n
 	end
 end
 
-class User
-	attr_accessor :name, :move
-
-	def initialize
-		@name = "User"
-	end
-
+class Computer < Player
+	# Generates a random move.
 	def make_move
-		puts "Choose one: (P/R/S)"
+		self.hand = Hand.new(Game::CHOICES.keys.sample)
+	end
+end
+
+class User < Player
+	# Asks user for his/her move.
+	def make_move
 		# Check if move is valid.
 		begin
+			puts "Choose one: (P/R/S)"
 			u_move = gets.chomp.upcase
-		end until ["P", "R", "S"].include?(u_move)
-		self.move = u_move
+		end until Game::CHOICES.keys.include?(u_move)
+		self.hand = Hand.new(u_move)
 	end
 end
 
 class Game
+
+	CHOICES = {"P" => "Paper", "R" => "Rock", "S" => "Scissors"}
+
 	attr_accessor :p1, :p2, :winner
 
-	def initialize(p1, p2)
-		@p1 = p1
-		@p2 = p2
+	def initialize
+		@p1 = User.new("User")
+		@p2 = Computer.new("Computer")
 		@winner = "No one"
 	end
 
@@ -51,31 +75,18 @@ class Game
 		end
 	end
 
-	# Compare 2 player's moves and updates the winner.
+	# Compares 2 player's hands and finds the winner.
 	def compare(p1, p2)
-		if p1.move == p2.move
-			self.winner = "No one"
-		elsif p1.move == "P"
-			if p2.move == "R"
-				self.winner = p1
-			else
-				self.winner = p2
-			end
-		elsif p1.move == "R"
-	    if p2.move == "P"
-	      self.winner = p2
-	    else
-	      self.winner = p1
-	    end
-		 elsif p1.move == "S"
-	    if p2.move == "R"
-	      self.winner = p2
-	    else
-	      self.winner = p1
-	    end
-	   end
+		if p1.hand > p2.hand
+		self.winner = p1
+	elsif p2.hand > p1.hand
+		self.winner = p2
+	else
+		self.winner = "No one"
+		end
 	end
 
+	# Launches game.
 	def play
 		loop do
 			puts "Play Paper Rock Scissors!"
@@ -83,7 +94,7 @@ class Game
 			# Both players make move.
 			p1.make_move
 			p2.make_move
-			puts "#{p1.name} picked #{to_word(p1.move)} and #{p2.name} picked #{to_word(p2.move)}."
+			puts "#{p1.name} picked #{Game::CHOICES[p1.hand.move]} and #{p2.name} picked #{Game::CHOICES[p2.hand.move]}."
 
 			# Compare and find the winner
 			compare(p1, p2)
@@ -92,35 +103,19 @@ class Game
 			self.say_winner
 
 			# Asks if user wants to play again.
-			puts "Play again? (Y/N)"
-
 			begin
+				puts "Play again? (Y/N)"
 				choice = gets.chomp.upcase
   		end until ["Y", "N"].include?(choice)
 
   		if choice == "N"
   			break
   		end
+
   	end
   end
 
-  private
-
-  # Takes a code (P/R/S) and converts it to the words "Paper", "Rock" or "Scissors".
-	def to_word(code)
-	  case code
-	  when "P"
-	    "Paper"
-	  when "R"
-	    "Rock"
-	  when "S"
-	    "Scissors"
-	  end
-	end
-
 end
 
-p1 = User.new
-p2 = Computer.new
-game = Game.new(p1, p2)
+game = Game.new
 game.play
